9c18ff842da65d4495aa9ed1060289dab6dec5a3
X-Slasher <148427695+X-S-L-A-S-H-E-R@users.noreply.github.com>
2024-05-05 16:50:17 +0600

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,100 +1,102 @@
 const {
-	Function,
-	isPublic,
-	Fancy,
-	prefix,
-	parsedUrl,
-	formatp,
-	commands
+    Function,
+    isPublic,
+    Fancy,
+    prefix,
+    parsedUrl,
+    formatp,
+    commands
 } = require('../lib/');
+
 const {
-	BOT_INFO,
-	MODE,
-	PREFIX,
-	VERSION
-} = require('../config')
-const os = require('os')
-const speed = require('performance-now')
+    BOT_INFO,
+    VIDEO_MENU,
+    MODE,
+    PREFIX,
+    VERSION
+} = require('../config');
+
+const os = require('os');
+const speed = require('performance-now');
+
 Function({
-	pattern: 'menu',
-	fromMe: isPublic,
-	type: 'info'
+    pattern: 'menu',
+    fromMe: isPublic,
+    type: 'info'
 }, async (message, match, client) => {
-	const commandslist = {}
-	commands.map(async (command, index) => {
-		if (command.dontAddCommandList === false && command.pattern !== undefined) {
-			try {
-				var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
-				var mmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
-			} catch {
-				var match = [command.pattern];
-			}
+    const commandslist = {};
+    for (const command of commands) {
+        if (command.dontAddCommandList === false && command.pattern !== undefined) {
+            try {
+                var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                var mmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+            } catch (error) {
+                var match = [command.pattern];
+            }
 
-			var HANDLER = '';
+            var HANDLER = '';
 
-			if (/\[(\W*)\]/.test(PREFIX)) {
-				HANDLER = PREFIX.match(/\[(\W*)\]/)[1][0];
-			} else {
-				HANDLER = '.';
-			}
-			if (!commandslist[command.type]) commandslist[command.type] = []
-			commandslist[command.type].push((match.length >= 3 ? (HANDLER + mmatch) : command.pattern).trim())
-		}
-	})
-	let msg = `╭━━━〔 ${BOT_INFO.split(";")[0]} ⁩〕━━━┈⊷
-┃✵╭──────────────
-┃✵│ Owner : ${BOT_INFO.split(";")[1]}
-┃✵│ User : ${m.pushName.replace( /[\r\n]+/gm, "" )}
-┃✵│ Plugins : ${commands.length}
-┃✵│ Runtime : ${runtime(process.uptime())}
-┃✵│ Mode : ${MODE}
-┃✵│ Platform : ${os.platform()}
-┃✵│ Ram : ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
-┃✵│ Version : ${VERSION}
-┃✵╰──────────────
-╰━━━━━━━━━━━━━━━┈⊷
-`
-	for (const command in commandslist) {
-		msg += `╭─────────────┈⊷
-`
-		msg += `│ 「 *${await Fancy(command.toUpperCase(), 32)}* 」 `
-		msg += `╰┬────────────┈⊷\n┌┤\n`
-		for (const plugin of commandslist[command])
-			msg += `││◦➛ ${await Fancy(plugin.toLowerCase(), 32)}\n`
-		msg += `│╰────────────┈⊷
-`
-		msg += `╰─────────────┈⊷
-`
-	}
-	await message.send(msg);
-	/* var img = await parsedUrl(BOT_INFO)
-	if (img.length == 0) {
-		img = ['https://i.imgur.com/qJUBCYm.jpeg']
-	}
-	const image = img[Math.floor(Math.random() * img.length)]
-	const type = image.endsWith('mp4') ? 'video' : 'image'
-	const buttonMessage = {
-		[type]: { url: image },
-		caption: `${msg}`,
-		footer: `${BOT_INFO.split(";")[0] || ' '}`,
-		buttons: [{buttonId: prefix + 'ping', buttonText: { displayText: 'Speed Test' }, type: 1},{ buttonId: prefix + 'list', buttonText: { displayText: 'List Commands' }, type: 1}]
-	}
-	await message.client.sendMessage(message.chat, buttonMessage)
-	*/
-	
+            if (/\[(\W*)\]/.test(PREFIX)) {
+                HANDLER = PREFIX.match(/\[(\W*)\]/)[1][0];
+            } else {
+                HANDLER = '.';
+            }
+            if (!commandslist[command.type]) commandslist[command.type] = [];
+            commandslist[command.type].push((match.length >= 3 ? (HANDLER + mmatch) : command.pattern).trim());
+        }
+    }
+
+    let mono = "```";
+    let msg = `
+ᴋᴏɴɴɪᴄʜɪᴡᴀ *${message.pushName.replace(/[\r\n]+/gm, "")}*..! ​ɪᴍ ɪʀɪs-ᴍᴅ 🍀, ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ᴠᴇɴᴏxsᴇɴᴘᴀɪ..!\n\n
+「 𝙎𝙮𝙨𝙩𝙚𝙢 𝙄𝙣𝙛𝙤 」\n
+𝘽𝙤𝙩 𝙉𝙖𝙢𝙚: ${BOT_INFO.split(";")[0]}
+𝙈𝙤𝙙𝙚: ${MODE}
+𝙋𝙡𝙪𝙜𝙞𝙣𝙨: ${commands.length}
+𝙋𝙧𝙚𝙛𝙞𝙭: [ *${PREFIX}* ]
+𝙐𝙥𝙩𝙞𝙢𝙚: ${runtime(process.uptime())}
+𝙍𝙖𝙢: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+𝙇𝙞𝙗𝙧𝙖𝙧𝙮: Baileys-Md
+`;
+    for (const commandType in commandslist) {
+        msg += `╭───────────────────
+`;
+        msg += `│ 「 *${await Fancy(commandType.toUpperCase(), 32)}* 」 `;
+        msg += `╰┬──────────────────\n┌┤\n`;
+        for (const plugin of commandslist[commandType]) {
+            msg += `││◦ ${mono}${await Fancy(plugin.toLowerCase(), 32)}${mono}\n`;
+        }
+        msg += `│╰──────────────\n`;
+        msg += `╰───────────────\n`;
+    }
+
+    var vid = await parsedUrl(VIDEO_MENU);
+    if (vid.length === 0) {
+        vid = ['https://i.imgur.com/s1XbCFo.mp4'];
+    }
+    const video = vid[Math.floor(Math.random() * vid.length)];
+    const type = video.endsWith('mp4') ? 'video' : 'image';
+    const buttonMessage = {
+        [type]: { url: video },
+        gifPlayback: true,
+        caption: `${msg}`,
+        footer: `${BOT_INFO.split(";")[0] || ' '}`,
+        buttons: [{ buttonId: prefix + 'ping', buttonText: { displayText: 'Speed Test' }, type: 1 }, { buttonId: prefix + 'list', buttonText: { displayText: 'List Commands' }, type: 1 }]
+    };
+    await message.client.sendMessage(message.chat, buttonMessage);
 });
 
 const runtime = function(seconds) {
-	seconds = Number(seconds);
-	var d = Math.floor(seconds / (3600 * 24));
-	var h = Math.floor(seconds % (3600 * 24) / 3600);
-	var m = Math.floor(seconds % 3600 / 60);
-	var s = Math.floor(seconds % 60);
-	var dDisplay = d > 0 ? d + (d == 1 ? " d " : " d ") : "";
-	var hDisplay = h > 0 ? h + (h == 1 ? " h " : " h ") : "";
-	var mDisplay = m > 0 ? m + (m == 1 ? " m " : " m ") : "";
-	var sDisplay = s > 0 ? s + (s == 1 ? " s" : " s") : "";
-	return dDisplay + hDisplay + mDisplay + sDisplay;
-}
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor(seconds % (3600 * 24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+    var dDisplay = d > 0 ? d + (d == 1 ? " d " : " d ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " h " : " h ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " m " : " m ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " s" : " s") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+};
 
 exports.runtime = runtime;
